7499d7ed5dc3a78bed633394d336f1d173a44793
aal80 <anton.al80@gmail.com>
2026-03-13 16:43:52 -0500

    updates - empty shell

---
+++ empty-shell-with-agentcore-sdk/terraform/ecr.tf
@@ -0,0 +1,20 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
+data "aws_ecr_repository" "this" {
+  name = local.ecr_repository_name
+}
+
+data "aws_ecr_image" "this" {
+  repository_name = data.aws_ecr_repository.this.name
+  image_tag = "latest"
+}
+
+locals {
+  full_ecr_image_uri_with_digest = "${data.aws_ecr_repository.this.repository_url}@${data.aws_ecr_image.this.image_digest}"
+}
+
+output "full_ecr_image_uri_with_digest" {
+  value = local.full_ecr_image_uri_with_digest
+}
